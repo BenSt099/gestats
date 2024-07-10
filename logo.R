2dd701b8c45cc5c098999e51cfbe119f3e97e974
BenSt099 <ben.steinhauer.de@gmail.com>
2024-07-10 22:38:00 +0200

    added logo

---
+++ logo.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+GE_States <- gisco_get_nuts(
+  nuts_level = 1,
+  resolution = 10,
+  country = "Germany",
+  year = 2021
+)
+
+data <- read.table(text = "
+      state value
+      Sachsen 1
+      Bayern 1
+      Rheinland-Pfalz 1
+      Saarland 1
+      Schleswig-Holstein 2
+      Niedersachsen  2
+      Nordrhein-Westfalen 2
+      Brandenburg 2
+      Baden-Württemberg 2
+      Hamburg 2
+      Mecklenburg-Vorpommern 2
+      Hessen 3
+      Berlin 3
+      Thüringen 3
+      Bremen 2
+      Sachsen-Anhalt 3
+", header = T)
+
+states <- GE_States %>%
+  left_join(data, by = c("NUTS_NAME" = "state"))
+colors <- c("black", "red2", "black", "black", "red2", "red2", "red2", "red2", "black", "gold2", "gold2", "black", "black", "black", "black", "red2")
+
+ggplot(states) +
+  geom_sf(fill = colors)
